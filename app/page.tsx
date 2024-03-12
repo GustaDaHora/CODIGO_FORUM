b4dee16ec3251685a5031accc8e33db411550cda
FelipeDoSax <felipe85soares@gmail.com>
2024-03-12 14:58:39 -0300

    Limpei 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { Suspense } from 'react'
-import TablePlaceholder from '@/components/table-placeholder'
-import ExpandingArrow from '@/components/expanding-arrow'
 
-export const dynamic = 'force-dynamic'
 
 export default function Home() {
   
