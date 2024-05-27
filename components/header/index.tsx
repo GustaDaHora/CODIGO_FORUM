65b1d5be6a0981044e8e8f22ab937f61c6e74660
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2024-05-27 16:04:23 -0400

    Update Header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,28 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "@/components/logo";
 import "./index.css";
-import { FiLogIn } from "react-icons/fi";
+import { FiLogIn, FiLogOut } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    setIsAuthenticated(!!token);
+  }, []);
+
+  const handleSignout = () => {
+    Cookies.remove("token");
+    setIsAuthenticated(false);
+    router.replace("/"); // Redirect to home page or login page after signout
+  };
+
   return (
     <header className="header">
       <div className="conteudoHeader">
@@ -18,14 +37,30 @@ export default function Header() {
           <a href="/">
             <button className="botaoHeader">Sem Resposta</button>
           </a>
-          <Link href="/auth/register" passHref>
-            CADASTRA
-          </Link>
-          <Link href="/auth/signin" passHref>
-            <FiLogIn className="iconeLogin" />
-            Login
-          </Link>
-          <Link href="/profile">Meu Perfil</Link>
+          {!isAuthenticated && (
+            <>
+              <Link href="/auth/register" passHref>
+                <button className="botaoHeader">Cadastro</button>
+              </Link>
+              <Link href="/auth/signin" passHref>
+                <button className="botaoHeader">
+                  <FiLogIn className="iconeLogin" />
+                  Login
+                </button>
+              </Link>
+            </>
+          )}
+          {isAuthenticated && (
+            <>
+              <Link href="/profile" passHref>
+                <button className="botaoHeader">Meu Perfil</button>
+              </Link>
+              <button className="botaoHeader" onClick={handleSignout}>
+                <FiLogOut className="iconeLogin" />
+                Sign Out
+              </button>
+            </>
+          )}
         </nav>
       </div>
     </header>
