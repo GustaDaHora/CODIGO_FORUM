5c2203a07a8e535a60f9632d390d6daf11f98cfb
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2025-04-08 01:49:19 -0400

    Fixed Tailwind not working

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import "./globals.css";
 export const dynamic = "force-dynamic";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Main from "@/app/dashboard/page";
-import "./globals.css";
 
 export default function Home() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        lato: ['Lato', 'sans-serif'],
-      }         
-    },
+    extend: {},
   },
   plugins: [],
 }
